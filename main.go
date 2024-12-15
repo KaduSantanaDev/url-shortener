fb6417a94c916a855ba0fc66e56c10854030f893
KaduSantanaDev <kaduzera.ks@gmail.com>
2024-12-15 11:06:53 -0300

    feat: create app

--- main.go
+++ main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"url-shortner/database/config"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("cannot load .env file")
+	}
+
+	db, err := config.NewPostgres()
+	if err != nil {
+		log.Fatalf("failed to connect to db: %v", err)
+	}
+	log.Println("connected to db successfully:", db)
+
 	router := gin.Default()
+
 	router.GET("/ping", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	err := router.Run()
-	if err != nil {
-		fmt.Errorf("error: %v", err)
+
+	if err := router.Run(":8005"); err != nil {
+		log.Fatalf("failed to start server: %v", err)
 	}
 }
