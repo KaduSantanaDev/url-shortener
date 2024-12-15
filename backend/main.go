f0ad13a0027705d207c4d44fa377446b292057b2
KaduSantanaDev <kaduzera.ks@gmail.com>
2024-12-15 18:15:30 -0300

    chore: move all project files to backend folder

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"url-shortner/database/config"
+	"url-shortner/database/repository"
+	"url-shortner/handlers"
 )
 
 func main() {
@@ -17,15 +20,22 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to connect to db: %v", err)
 	}
-	log.Println("connected to db successfully:", db)
+	log.Println("connected to db successfully")
 
 	router := gin.Default()
+	router.Use(cors.Default())
+
+	linkRepo := repository.NewLinkRepository(db)
+	linkHandler := handlers.NewLinkHandler(linkRepo)
 
 	router.GET("/ping", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
+
+	router.POST("/links", linkHandler.CreateLink)
+	router.GET("/links/:slug", linkHandler.GetLink)
 
 	if err := router.Run(":8005"); err != nil {
 		log.Fatalf("failed to start server: %v", err)
