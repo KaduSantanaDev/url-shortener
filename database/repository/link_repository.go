165af86eee26fb58493a36fa07d07713c4db95a6
KaduSantanaDev <kaduzera.ks@gmail.com>
2024-12-15 11:06:05 -0300

    feat: create link repository

---
+++ database/repository/link_repository.go
@@ -0,0 +1,31 @@
+package repository
+
+import (
+	"gorm.io/gorm"
+	"url-shortner/database/models"
+)
+
+type LinkRepository interface {
+	CreateLink(link *models.Link) error
+	FindBySlug(slug string) (*models.Link, error)
+}
+
+type GormLinkRepository struct {
+	DB *gorm.DB
+}
+
+func NewLinkRepository(db *gorm.DB) LinkRepository {
+	return &GormLinkRepository{db}
+}
+
+func (repository *GormLinkRepository) CreateLink(link *models.Link) error {
+	return repository.DB.Create(link).Error
+}
+
+func (repository *GormLinkRepository) FindBySlug(slug string) (*models.Link, error) {
+	var link models.Link
+	if err := repository.DB.Where(&link, "slug = ?", slug).Error; err != nil {
+		return nil, err
+	}
+	return &link, nil
+}
