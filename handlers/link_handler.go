b231159739ea57fdd787d87156ab21e54a93667d
KaduSantanaDev <kaduzera.ks@gmail.com>
2024-12-15 17:57:34 -0300

    fix: fix createLink and getLink

--- handlers/link_handler.go
+++ handlers/link_handler.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"url-shortner/database/models"
 	"url-shortner/database/repository"
 )
 
@@ -28,12 +30,20 @@ func (handler *LinkHandler) CreateLink(context *gin.Context) {
 		})
 		return
 	}
+	fmt.Println(body)
 
-	existingLink, _ := handler.repo.FindBySlug(body.Slug)
-	if existingLink != nil {
-		context.JSON(400, gin.H{
-			"error": "link already exists",
+	link := &models.Link{
+		Url:      body.URL,
+		Slug:     body.Slug,
+		ShortUrl: context.Request.Host + "/" + body.Slug,
+	}
+
+	err := handler.repo.CreateLink(link)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
 		})
+		return
 	}
 
 	context.JSON(http.StatusCreated, gin.H{
@@ -41,26 +51,24 @@ func (handler *LinkHandler) CreateLink(context *gin.Context) {
 	})
 }
 
-type getLinkRequest struct {
-	Slug string `json:"slug" binding:"required,min=6"`
+type getLinkRequestURI struct {
+	Slug string `uri:"slug" binding:"required"`
 }
 
 func (handler *LinkHandler) GetLink(context *gin.Context) {
-	var body getLinkRequest
-	if err := context.ShouldBindJSON(&body); err != nil {
+	var uri getLinkRequestURI
+	if err := context.ShouldBindUri(&uri); err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{
 			"error": err.Error(),
 		})
 		return
 	}
-	link, err := handler.repo.FindBySlug(body.Slug)
-	if err != nil {
-		context.JSON(http.StatusNotFound, gin.H{})
-		return
-	}
-	_, err = http.Get(link.Url)
-	if err != nil {
-		context.JSON(http.StatusNotFound, gin.H{"error": "Error while trying to access the link URL"})
+	link, err := handler.repo.FindBySlug(uri.Slug)
+	if link.Slug != uri.Slug || err != nil {
+		context.JSON(http.StatusNotFound, gin.H{
+			"error": "link not found",
+		})
+
 		return
 	}
 
