45464e498342463912dfd6d20e49aa3d53476ca2
KaduSantanaDev <kaduzera.ks@gmail.com>
2024-12-14 22:20:14 -0300

    chore(deps): update dependencies
    add gorm

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	err := router.Run()
+	if err != nil {
+		fmt.Errorf("error: %v", err)
+	}
 }
